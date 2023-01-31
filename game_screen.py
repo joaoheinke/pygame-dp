4873c929abdc3882b2f0c9ca426d0d05ad5b2409
Leonardo Ponciano <leonardops3@al.insper.edu.br>
2023-01-31 11:23:22 -0300

    passo 6

--- game_screen.py
+++ game_screen.py
@@ -11,7 +11,8 @@ def game_screen(window):
     DONE = 0
     PLAYING = 1
     state = PLAYING
-    
+    pontos=0
+
     lista_de_imagens= pygame.sprite.Group()
     for i in range(5):
 
@@ -20,11 +21,10 @@ def game_screen(window):
 
     # ===== Loop principal =====
     while state != DONE:
-        pontos=0
-        font = pygame.font.SysFont(None, 80)
-        text = font.render(f'PONTOS: {pontos} ', True, (0, 0, 255))
+        font = pygame.font.SysFont(None, 40)
+        text = font.render(f'PONTOS: {pontos} ', True, (255, 255, 255))
         clock.tick(FPS)
-        window.blit(text, (10,10))
+        vidas=0
             
             # ----- Trata eventos
         for event in pygame.event.get():
@@ -37,11 +37,12 @@ def game_screen(window):
                         foto.kill()
                         chiuaua= ChihuahuaOrMuffin(dicionario_de_arquivos)
                         lista_de_imagens.add(chiuaua)
-                        if foto in dicionario_de_arquivos['Chihuahua']:
+                        if foto.chiuaua==True:
                             pontos+=1
                         else:
                             chiuaua= ChihuahuaOrMuffin(dicionario_de_arquivos)
                             lista_de_imagens.add(chiuaua)
+                    
 
 
 
@@ -52,6 +53,7 @@ def game_screen(window):
         window.fill(BLACK)  # Preenche com a cor branca
         lista_de_imagens.update()
         lista_de_imagens.draw(window)
+        window.blit(text, (10,10))
         pygame.display.update()  # Mostra o novo frame para o jogador
 
     return state
